e008ec3164b28e2a4c877a7fbce9d8150e0e1140
Rubén Darío González <71092701+gonzalezruben681@users.noreply.github.com>
2023-09-06 16:05:31 -0600

    add: custom hook y organización del proyecto

--- src/App.js
+++ src/App.js
@@ -1,34 +1,59 @@
 import React, { useState } from "react";
-import { CreateTodoButton } from "./CreateTodoButton";
+import { CreateTodoButton } from "./CreateTodoButton/CreateTodoButton";
 import { TodoCounter } from "./TodoCounter";
 import { TodoItem } from "./TodoItem";
 import { TodoList } from "./TodoList";
 import { TodoSearch } from "./TodoSearch";
 
-const defaultTodos = [
-  {text:'Cortar', completed: true},
-  {text:'Tomar el curso de Intro a React.js', completed: false},
-  {text:'Llorar con la Llorona', completed: false},
-  {text:'sfgsf', completed: false},
-  {text:'hola', completed: false},
-];
+// const defaultTodos = [
+//   {text:'Cortar', completed: true},
+//   {text:'Tomar el curso de Intro a React.js', completed: false},
+//   {text:'Llorar con la Llorona', completed: false},
+//   {text:'sfgsf', completed: false},
+//   {text:'hola', completed: false},
+// ];
+
+// EL CUSTOM HOOKS
+function useLocalStorage(itemName, initialValue) {
+  const localStorageItem = localStorage.getItem(itemName);
+  let parsedItem;
+  
+  if (!localStorageItem) {
+    localStorage.setItem(itemName, JSON.stringify(initialValue));
+    parsedItem = initialValue;
+  } else {
+    parsedItem = JSON.parse(localStorageItem);
+  }
+
+  const [item, setItem] = useState(parsedItem);
+
+  const saveItem = (newItem) => {
+    localStorage.setItem(itemName, JSON.stringify(newItem));
+    setItem(newItem)
+  };
+  return [item, saveItem];
+}
+
 
 function App() {
-  const [todos, setTodos] = useState(defaultTodos);
+
+  //UTILIZANDO EL CUSTOM HOOKS
+  const [todos, saveTodos] = useLocalStorage('TODOS_V1', []);
   const [searchValue, setSearchValue] = useState('');
+
   
   const completeTodo = (text) => {
     const newTodos = [...todos];
     const index = newTodos.findIndex( todo => todo.text === text);
     newTodos[index].completed=true;
-    setTodos(newTodos);
+    saveTodos(newTodos);
   }
 
   const deleteTodo = (text) => {
     const newTodos = [...todos];
     const index = newTodos.findIndex( todo => todo.text === text);
     newTodos.splice(index,1);
-    setTodos(newTodos); 
+    saveTodos(newTodos); 
   }
   
   // estados derivados del estado de hooks
